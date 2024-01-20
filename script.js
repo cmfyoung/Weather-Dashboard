10a37580bab2c07dd999552555b41c24f2fc6e45
cmfyoung <cmfyoung7@gmail.com>
2024-01-20 16:00:29 -0500

    Fixed the city search buttons

--- script.js
+++ script.js
@@ -16,21 +16,20 @@ var lon;
 
 
 // Search Weather function pulling weather data from open weather 
-function searchWeather () {
-    const cityName = city.value.trim();
-    if(!cityName) return;
+function searchWeather(cityName) {
+    if (!cityName) return;
 
     console.log(cityName);
 
-// API request for Current Weather
+    // API request for Current Weather
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-    
-// Fetch current weather data 
+
+    // Fetch current weather data 
     fetch(requestUrl)
-        .then ((response) => {
-        return response.json();
+        .then((response) => {
+            return response.json();
         })
- // Display Current weather 
+        // Display Current weather 
         .then((data) => {
             lat = data.coord.lat;
             lon = data.coord.lon;
@@ -43,22 +42,22 @@ function searchWeather () {
             currentHumidity.textContent = `Humidity: ${humidity}%`;
             //API for 5-day forecast
             const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-         
-        //fetch 5-day data
-        fetch(forecastUrl)
-        .then ((response) => {
-        return response.json();
-        })
 
-        .then((data) => {
-            console.log(data)
-            displayForecast(data.list);
-        })
-            
-            });
-        
-// Save city to local Storage
-saveCity(cityName);
+            //fetch 5-day data
+            fetch(forecastUrl)
+                .then((response) => {
+                    return response.json();
+                })
+
+                .then((data) => {
+                    console.log(data)
+                    displayForecast(data.list);
+                })
+
+        });
+
+    // Save city to local Storage
+    saveCity(cityName);
 }
 
 // display the 5 day forecast
@@ -66,62 +65,71 @@ saveCity(cityName);
 function displayForecast(forecastList) {
     forecast.empty();
     console.log(forecastList)
-    for(var i = 0; i < forecastList.length; i += 8 ) {
-        const forecastItem = forecastList [i];
+    for (var i = 0; i < forecastList.length; i += 8) {
+        const forecastItem = forecastList[i];
         console.log(i)
 
         //create HTML elements for the forecast 
-    var dayEl=`<div class="card row my-1 mx-auto" id="forecast">
+        var dayEl = `<div class="card row my-1 mx-auto" id="forecast">
     <ul id="Day-1"></ul>
     </div>`;
-    var divEl = $("<div>");
-    divEl.addClass("card row my-1 mx-auto");
-    var ulEl = $("<ul>");
-//display the day number for the 5day forecast
-    var dayText= `Day ${Math.ceil((i+1)/8)}`;
-    var dayNum = $("<li>");
-    dayNum.text(dayText);
-    dayNum.addClass("forecast-day");
-    ulEl.append(dayNum);
-//Forecast Details 
-    var temp = $("<li>");
-    var wind = $("<li>");
-    var humidity = $("<li>");
-    temp.text(`Temperature: ${forecastItem.main.temp} °F`);
-    wind.text(`Wind: ${forecastItem.wind.speed} mph`);
-    humidity.text(`Humidity: ${forecastItem.main.humidity}%`);
-    ulEl.append(temp, wind, humidity);
-    console.log(ulEl)
-    divEl.append(ulEl);
-    forecast.append(ulEl);
+        var divEl = $("<div>");
+        divEl.addClass("card row my-1 mx-auto");
+        var ulEl = $("<ul>");
+        //display the day number for the 5day forecast
+        var dayText = `Day ${Math.ceil((i + 1) / 8)}`;
+        var dayNum = $("<li>");
+        dayNum.text(dayText);
+        dayNum.addClass("forecast-day");
+        ulEl.append(dayNum);
+        //Forecast Details 
+        var temp = $("<li>");
+        var wind = $("<li>");
+        var humidity = $("<li>");
+        temp.text(`Temperature: ${forecastItem.main.temp} °F`);
+        wind.text(`Wind: ${forecastItem.wind.speed} mph`);
+        humidity.text(`Humidity: ${forecastItem.main.humidity}%`);
+        ulEl.append(temp, wind, humidity);
+        console.log(ulEl)
+        divEl.append(ulEl);
+        forecast.append(ulEl);
     }
 
 }
 
 // Save City to local Storage
 function saveCity(cityName) {
-    savedCitiesEl.empty() 
-    let savedCities = JSON.parse(localStorage.getItem("cities")) || []
-    
-    if (cityName){
-    if (!savedCities.includes(cityName)) {
-        savedCities.push(cityName);
-        localStorage.setItem("cities", JSON.stringify(savedCities));
-    }}
-    console.log(savedCities)
-    for (var i=0; i < savedCities.length; i++) {
-    var savedCitiesbtn = $("<button>");
-    savedCitiesbtn.text(savedCities[i]);
-    savedCitiesbtn.addClass("saved-city-button");
-    savedCitiesbtn.on("click", function () {
-        var cityName = $(this).text();
-        searchWeather(cityName);
-    })
-    savedCitiesEl.append(savedCitiesbtn);
+    savedCitiesEl.empty();
+
+    let savedCities = JSON.parse(localStorage.getItem("cities")) || [];
+
+    if (cityName) {
+        if (!savedCities.includes(cityName)) {
+            savedCities.push(cityName);
+            localStorage.setItem("cities", JSON.stringify(savedCities));
+        }
+    }
+    console.log(savedCities);
+
+    for (var i = 0; i < savedCities.length; i++) {
+        var savedCitiesbtn = $("<button>");
+        savedCitiesbtn.text(savedCities[i]);
+        savedCitiesbtn.addClass("saved-city-button");
+        savedCitiesbtn.on("click", cityClick);
+        savedCitiesEl.append(savedCitiesbtn);
     }
 }
 
 saveCity();
+
+savedCitiesEl.on("click", ".saved-city-button", cityClick);
+
+function cityClick() {
+    console.log("button clicked!!!")
+    var cityName = $(this).text();
+    searchWeather(cityName);
+}
+
 
 // Clear the saved cities
 function clearHistory() {
@@ -129,9 +137,13 @@ function clearHistory() {
 
     // clear local storage
     localStorage.removeItem("cities")
+    savedCitiesEl.empty(); 
 }
 
-// savedCitiesEl.addEventListener("click", )
 
-searchButton.addEventListener("click", searchWeather); 
+
+searchButton.addEventListener("click", function () {
+    var cityName = city.value.trim();
+    searchWeather(cityName);
+});
 clearButton.addEventListener("click", clearHistory);
